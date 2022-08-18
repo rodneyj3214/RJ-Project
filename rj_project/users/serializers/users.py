493ad7ef0c45c7603ce04126945d02b2bc49fa75
Rodney Jácome <rodneyj3214@gmail.com>
2022-08-18 09:08:36 -0500

    RJ - Api View

---
+++ rj_project/users/serializers/users.py
@@ -0,0 +1,33 @@
+from django.contrib.auth import authenticate
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+
+from rj_project.users.models import User
+
+
+class UserModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "phone_number",
+        )
+
+
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(min_length=8, max_length=64)
+
+    def validate(self, data):
+        user = authenticate(username=data["email"], password=data["password"])
+        if not user:
+            raise serializers.ValidationError("Invalid Credentials")
+        self.context["user"] = user
+        return data
+
+    def create(self, data):
+        token, created = Token.objects.get_or_create(user=self.context["user"])
+        return self.context["user"], token.key
--- rj_project/users/urls.py
+++ rj_project/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from rj_project.users.views import (
+from rj_project.users.views.users import (
+    UserAPIView,
     user_detail_view,
     user_redirect_view,
     user_update_view,
@@ -8,6 +9,7 @@ from rj_project.users.views import (
 
 app_name = "users"
 urlpatterns = [
+    path("create", UserAPIView),
     path("~redirect/", view=user_redirect_view, name="redirect"),
     path("~update/", view=user_update_view, name="update"),
     path("<str:username>/", view=user_detail_view, name="detail"),
