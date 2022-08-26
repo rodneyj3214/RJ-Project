284997aef2ecd5e93a53cfd6664135691b7ed5d8
Rodney Jácome <rodneyj3214@gmail.com>
2022-08-26 00:11:21 -0500

    RJ - Retrieve for user

--- rj_project/users/views/users.py
+++ rj_project/users/views/users.py
@@ -4,11 +4,15 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import DetailView, RedirectView, UpdateView
-from rest_framework import status, viewsets
+from rest_framework import mixins, status, viewsets
 from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from rj_project.circles.models import Circle
+from rj_project.circles.serializers.circles import CircleModelSerializer
+from rj_project.users.permissions import IsAccountOwner
 from rj_project.users.serializers.users import (
     AccountVerificationSerializer,
     UserLoginSerializer,
@@ -19,8 +23,21 @@ from rj_project.users.serializers.users import (
 User = get_user_model()
 
 
-class UserViewSet(viewsets.GenericViewSet):
+class UserViewSet(mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     """User ViewSet"""
+
+    queryset = User.objects.filter(is_active=True, is_client=True)
+    serializer_class = UserModelSerializer
+    lookup_field = "username"
+
+    def get_permissions(self):
+        if self.action in ["signup", "login", "verify"]:
+            permissions = [AllowAny]
+        elif self.action == "retrieve":
+            permissions = [IsAuthenticated, IsAccountOwner]
+        else:
+            permissions = [IsAuthenticated]
+        return [permission() for permission in permissions]
 
     @action(detail=False, methods=["post"])
     def signup(self, request):
@@ -45,6 +62,18 @@ class UserViewSet(viewsets.GenericViewSet):
         serializer.save()
         data = {"message": "Congratulations, now go share some rides!"}
         return Response(data, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, *args, **kwargs):
+        response = super().retrieve(request, *args, **kwargs)
+        circles = Circle.objects.filter(
+            members=request.user, membership__is_active=True
+        )
+        data = {
+            "user": response.data,
+            "circles": CircleModelSerializer(circles, many=True).data,
+        }
+        response.data = data
+        return response
 
 
 class UserAPIView(APIView):
